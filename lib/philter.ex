9ba2a3e1472b2801c38d3b6970751cb80477af0e
Dubois Grayson <Grayson.Dubois@hcahealthcare.com>
2018-06-01 12:34:07 -0500

    Created new elixir project

---
+++ lib/philter.ex
@@ -0,0 +1,18 @@
+defmodule Philter do
+  @moduledoc """
+  Documentation for Philter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Philter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/philter_test.exs
@@ -0,0 +1,8 @@
+defmodule PhilterTest do
+  use ExUnit.Case
+  doctest Philter
+
+  test "greets the world" do
+    assert Philter.hello() == :world
+  end
+end
